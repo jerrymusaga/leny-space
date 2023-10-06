74611e62084eb96d8030ea1b86dc456991321537
jerrymusaga <musagatechnology@gmail.com>
2023-10-06 05:50:11 +0100

    explore publications from lens api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import {
+  useExplorePublicationsQuery,
+  PublicationSortCriteria,
+} from "../graphql/generated";
 export default function Home() {
+  const { data, isLoading, error } = useExplorePublicationsQuery(
+    {
+      endpoint: "https://api.lens.dev",
+      fetchParams: {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    },
+    {
+      request: {
+        sortCriteria: PublicationSortCriteria.Latest,
+      },
+    },
+    {
+      // Don't refetch the user comes back
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    }
+  );
+
+  console.log({ data, isLoading, error });
+
   return <>Hello world</>;
 }
